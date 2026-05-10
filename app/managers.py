ef22a104b33eab45e621541607bc0cc78f7e9a69
Ostapenko Denys <wachax52@gmail.com>
2026-05-10 14:42:46 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -1,9 +1,10 @@
 import sqlite3
+from typing import List
 from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.table_name = table_name
         self.connection = sqlite3.connect(db_name)
         self.connection.execute(
@@ -14,15 +15,16 @@ class ActorManager:
         )
         self.connection.commit()
 
-    def create(self, first_name: str, last_name: str):
-        self.connection.execute(
+    def create(self, first_name: str, last_name: str) -> int:
+        cursor = self.connection.execute(
             f"INSERT INTO {self.table_name} "
             f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self.connection.commit()
+        return cursor.lastrowid
 
-    def all(self) -> list[Actor]:
+    def all(self) -> List[Actor]:
         cursor = self.connection.execute(
             f"SELECT id, first_name, last_name FROM {self.table_name}"
         )
@@ -32,17 +34,19 @@ class ActorManager:
                   first_name=row[1],
                   last_name=row[2]) for row in rows]
 
-    def update(self, pk: int, new_first_name: str, new_last_name: str):
-        self.connection.execute(
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> bool:
+        cursor = self.connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? WHERE id = ?",
             (new_first_name, new_last_name, pk)
         )
         self.connection.commit()
+        return cursor.rowcount > 0
 
-    def delete(self, pk: int):
-        self.connection.execute(
+    def delete(self, pk: int) -> bool:
+        cursor = self.connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (pk,)
         )
         self.connection.commit()
+        return cursor.rowcount > 0
